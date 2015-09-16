b6e7e5a605925415f62f07699383aa5a125187be
MikeDulik <mikedulik@gmail.com>
2015-09-16 18:21:15 -0400

    cant get request to stars/stars.json to work, hardcoding array in controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,18 +4,10 @@
 
 var starsApp = angular.module('starsApp', []);
 
-starsApp.controller('StarListCtrl', function($scope) {
-  $scope.stars = [
-    {'name': 'Star 1',
-    'snippet': 'Star 1 info yayy',
-    'distance': 2343525},
-    {'name': 'Star 2',
-    'snippet': 'Star 2 stuff heyyooo',
-    'distance': 230472309840293},
-    {'name': 'Star 3',
-    'snippet': 'Star 3 information brooo',
-    'distance': 2355}
-  ];
-
+starsApp.controller('StarListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('/stars/stars.json').success(function(data){
+    $scope.stars = data;
+  });
+  //default val of orderProp
   $scope.orderProp = 'distance';
-});
+}]);
