d6b2b273d51c88e899acdf1b458e378986ffc550
MikeDulik <mikedulik@gmail.com>
2015-09-16 17:34:20 -0400

    added orderProp functionality

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,8 +7,15 @@ var starsApp = angular.module('starsApp', []);
 starsApp.controller('StarListCtrl', function($scope) {
   $scope.stars = [
     {'name': 'Star 1',
-    'snippet': 'Star 1 info yayy'},
+    'snippet': 'Star 1 info yayy',
+    'distance': 2343525},
     {'name': 'Star 2',
-    'snippet': 'Star 2 stuff heyyooo'}
+    'snippet': 'Star 2 stuff heyyooo',
+    'distance': 230472309840293},
+    {'name': 'Star 3',
+    'snippet': 'Star 3 information brooo',
+    'distance': 2355}
   ];
+
+  $scope.orderProp = 'distance';
 });
