561ea90c425b2383eda77f6e405bb0815b347b1d
MikeDulik <mikedulik@gmail.com>
2015-09-16 19:01:32 -0400

    added 9 stars to local file array

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,10 +4,24 @@
 
 var starsApp = angular.module('starsApp', []);
 
-starsApp.controller('StarListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('/stars/stars.json').success(function(data){
-    $scope.stars = data;
-  });
+starsApp.controller('StarListCtrl', function($scope){
+  $scope.stars = [
+    {
+      "distance": 0.0000158128,
+      "id": "the-sun",
+      "imageUrl": "img/stars/sun.jpg",
+      "name": "Sun",
+      "snippet": "The Sun is the star at the center of our Solar System and is by far the most important source of energy for life on Earth."
+    },
+    {
+      "distance": 16.77,
+      "id": "altair",
+      "imageUrl": "img/stars/altair.jpg",
+      "name": "Altair",
+      "snippet": "Altair is the brightest star in the constellation Aquila and the twelfth brightest star in the night sky."
+    }
+  ];
+
   //default val of orderProp
   $scope.orderProp = 'distance';
-}]);
+});
